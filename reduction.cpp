6ba7f398c5227a5793e372f278b8d663b13bd3a3
Gustavo Barbosa <gbdsantos@inf.ufpel.edu.br>
2019-06-04 21:05:24 -0300

    funcionando para (sum = sum op anything)

--- reduction.cpp
+++ reduction.cpp
@@ -29,6 +29,8 @@ static bool areSameVariable(const ValueDecl *First, const ValueDecl *Second) {
          First->getCanonicalDecl() == Second->getCanonicalDecl();
 }
 
+/*char accname[100];
+*/
 StatementMatcher LoopMatcher = forStmt(hasLoopInit(declStmt(
                                 hasSingleDecl(varDecl(hasInitializer(integerLiteral(equals(0)))).bind("initVarName")))),
                                 hasIncrement(unaryOperator(hasOperatorName("++"),
@@ -37,27 +39,56 @@ StatementMatcher LoopMatcher = forStmt(hasLoopInit(declStmt(
                                   binaryOperator(hasOperatorName("<"),
                                   hasLHS(ignoringParenImpCasts(declRefExpr(to(varDecl(hasType(isInteger())).bind("condVarName"))))),
                                   hasRHS(expr(hasType(isInteger())))))
-                                ).bind("forLoop");
+                                ).bind("forLoop");/*
 StatementMatcher arrayClause = arraySubscriptExpr(unless(hasDescendant(binaryOperator())),
                                 hasDescendant(declRefExpr(to(varDecl(hasType(isInteger())).bind("arrayIndex")))));
-StatementMatcher opClause[4] = {binaryOperator(hasOperatorName("+"),
+StatementMatcher opClause[6] = {binaryOperator(hasOperatorName("+"),
+                                  hasDescendant(arrayClause), hasDescendant(declRefExpr(to(varDecl().bind("accOperator")))), unless(hasDescendant(binaryOperator()))) ,
+                                binaryOperator(hasOperatorName("*"),
                                   hasDescendant(arrayClause), hasDescendant(declRefExpr(to(varDecl().bind("accOperator")))), unless(hasDescendant(binaryOperator()))) ,
                                 binaryOperator(hasOperatorName("-") ,
                                   hasDescendant(arrayClause), hasDescendant(declRefExpr(to(varDecl().bind("accOperator")))), unless(hasDescendant(binaryOperator()))) ,
                                 binaryOperator(hasOperatorName("&") ,
                                   hasDescendant(arrayClause), hasDescendant(declRefExpr(to(varDecl().bind("accOperator")))), unless(hasDescendant(binaryOperator()))) ,
                                 binaryOperator(hasOperatorName("|") ,
+                                  hasDescendant(arrayClause), hasDescendant(declRefExpr(to(varDecl().bind("accOperator")))), unless(hasDescendant(binaryOperator()))) ,
+                                binaryOperator(hasOperatorName("/") ,
                                   hasDescendant(arrayClause), hasDescendant(declRefExpr(to(varDecl().bind("accOperator")))), unless(hasDescendant(binaryOperator())))
 };
 StatementMatcher ReduceMatcher =
     binaryOperator(hasOperatorName("="), hasParent(compoundStmt(hasParent(LoopMatcher))) , hasRHS(anyOf(
-      opClause[0], opClause[1], opClause[2], opClause[3])), hasLHS(declRefExpr(to(varDecl().bind("accumulator"))))
+      opClause[0], opClause[1], opClause[2], opClause[3], opClause[4], opClause[5])), hasLHS(declRefExpr(to(varDecl().bind("accumulator"))))
     ).bind("reduce");
+
+StatementMatcher ReduceMatcher2 = 
+    binaryOperator(hasOperatorName("+="), hasParent(compoundStmt(hasParent(LoopMatcher))) , hasRHS(
+      hasDescendant(arrayClause)), hasLHS(declRefExpr(to(varDecl().bind("accumulator"))))
+    ).bind("reduce");
+*/
+/* ESSA PARTE AQUI FUNCIONARIA, MAS ESTOU COM PROBLEMA NO UNLESS
+StatementMatcher ReduceMatcher = 
+  binaryOperator(hasOperatorName("="), hasLHS(declRefExpr(to(varDecl().bind("accumulator")))),
+  hasDescendant(declRefExpr(to(varDecl(equalsBoundNode("accumulator"))))), 
+  unless(hasDescendant(binaryOperator(hasDescendant(declRefExpr(to(varDecl(equalsBoundNode("accumulator"))))))))).bind("reduce");
+*/
+/* NAO COMPILA, PROBLEMA NA ULTIMA LINHA
+StatementMatcher ReduceMatcher =
+  binaryOperator(hasOperatorName("="), hasLHS(declRefExpr(to(varDecl().bind("accumulator")))), 
+  hasDescendant(binaryOperator(hasLHS(declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))), 
+    unless(hasDescendant(binaryOperator(hasRHS(hasDescendant(declRefExpr(to(varDecl(equalsBoundNode("accumulator"))))))))))).bind("reduce");
+*/
+
+StatementMatcher ReduceMatcher =
+  binaryOperator(hasOperatorName("="),
+  hasLHS(declRefExpr(to(varDecl().bind("accumulator")))), hasParent(compoundStmt(hasParent(LoopMatcher))),
+  hasRHS(binaryOperator(hasLHS(hasDescendant(declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))))), 
+  unless(hasDescendant(binaryOperator(hasRHS(hasDescendant(declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))))))).bind("reduce");
 
 class LoopPrinter : public MatchFinder::MatchCallback {
 public :
   virtual void run(const MatchFinder::MatchResult &Result) {
       ASTContext *Context = Result.Context;
+      llvm::outs() << "Hello!!\n";
       const BinaryOperator *BO = Result.Nodes.getNodeAs<BinaryOperator>("reduce");
       // We do not want to convert header files!
       if(!BO || !Context->getSourceManager().isWrittenInMainFile(BO->getOperatorLoc()))
@@ -65,18 +96,21 @@ public :
       const VarDecl *IncVar = Result.Nodes.getNodeAs<VarDecl>("incVarName");
       const VarDecl *CondVar = Result.Nodes.getNodeAs<VarDecl>("condVarName");
       const VarDecl *InitVar = Result.Nodes.getNodeAs<VarDecl>("initVarName");
-      const VarDecl *ArrayIndex = Result.Nodes.getNodeAs<VarDecl>("arrayIndex");
+      /*const VarDecl *ArrayIndex = Result.Nodes.getNodeAs<VarDecl>("arrayIndex");
       const VarDecl *AccOperator = Result.Nodes.getNodeAs<VarDecl>("accOperator");
-      const VarDecl *Accumulator = Result.Nodes.getNodeAs<VarDecl>("accumulator");
+      const VarDecl *Accumulator = Result.Nodes.getNodeAs<VarDecl>("accumulator");*/
 
       if(!areSameVariable(IncVar, CondVar) || !areSameVariable(IncVar, InitVar)){
+        llvm::outs() << "for variables didnt match\n";
         return;
-      }else if(!areSameVariable(IncVar, ArrayIndex)){
+      }/*else if(!areSameVariable(IncVar, ArrayIndex)){
+        llvm::outs() << "array index didnt match\n";
         return;
       }else if(!areSameVariable(AccOperator, Accumulator)){
+        llvm::outs() << "accumulator didnt match\n";
         return;
-      }
-    //  BO->dump();
+      }*/
+      BO->dump();
       llvm::outs() << "Potential Reduce pattern, in the next line you'll find the file and the suspect line.\n";
       BO->getExprLoc().dump(Context->getSourceManager());
       llvm::outs() << "\n";
@@ -90,6 +124,7 @@ int main(int argc, const char **argv) {
   LoopPrinter Printer;
   MatchFinder Finder;
   Finder.addMatcher(ReduceMatcher, &Printer);
+  //Finder.addMatcher(ReduceMatcher2, &Printer);
 
   return Tool.run(newFrontendActionFactory(&Finder).get());
 }
