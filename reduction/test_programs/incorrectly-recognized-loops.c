a24eb3152ccbbe7d6f9ca1aef743acaaa44a761d
João Rezende <30845402+JoaoLRezende@users.noreply.github.com>
2020-10-05 12:13:57 -0300

    Create incorrectly-recognized-loops.c

---
+++ reduction/test_programs/incorrectly-recognized-loops.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define ARRAYSIZE 10
+
+int main(){
+    int array[ARRAYSIZE] = {0, 1 , 2, 3, 4, 5, 6, 7, 8, 9};
+    int sum = 0;
+    int x = 0;
+
+    // None!
+
+    return 0;
+}
