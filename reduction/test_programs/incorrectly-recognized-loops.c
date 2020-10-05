874e632157b72952a2300bb6b415d21aa8bba4e0
João Rezende <30845402+JoaoLRezende@users.noreply.github.com>
2020-10-05 14:01:48 -0300

    Update incorrectly-recognized-loops.c

--- reduction/test_programs/incorrectly-recognized-loops.c
+++ reduction/test_programs/incorrectly-recognized-loops.c
@@ -7,7 +7,19 @@ int main(){
     int sum = 0;
     int x = 0;
 
-    // None!
-
+    /* From NPB3.3.1/NPB3.3-SER/IS/is.c.
+     * Incorrectly recognized.
+     * This loop computes 2^(-23) and 2^23.
+     * Note that the counter variable, i, doesn't appear in the loop of the body.
+     * Also, each assignment involves no variable other than the potential accumulator itself.
+     * Perhaps we should start checking for those things.
+     */
+    double	R23 = 1.0, T23 = 1.0;
+    for (i=1; i<=23; i++)
+    {
+      R23 = 0.50 * R23;
+      T23 = 2.0 * T23;
+    }
+    
     return 0;
 }
