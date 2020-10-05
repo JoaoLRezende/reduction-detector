080b08fc414a00c8f8326ce11f15047aafddf1f0
João Rezende <30845402+JoaoLRezende@users.noreply.github.com>
2020-10-05 11:54:51 -0300

    Update test_program.cpp

--- reduction/test_programs/test_program.cpp
+++ reduction/test_programs/test_program.cpp
@@ -1,76 +1,91 @@
 #include <iostream>
 
 #define ARRAYSIZE 10
-//PROBLEMA ATUAL EH BASICAMENTE: QUANDO ENCONTRO UM ACUMULADOR EM POTENCIAL, DEVO CONSEGUIR VERIFICAR SE APARECE APENAS UMA VEZ NO 
-//RESTO DA EQUACAO, E DEPOIS VERIFICAR SE APARECE NOVAMENTE EM ALGUMA EQUACAO DENTRO DO FOR. MAS COMO GUARDAR ONDE DA EQUACAO/FOR QUE ENCONTREI O ACUMULADOR EM QUESTAO?
+
 int main(){
     int array[ARRAYSIZE] = {0, 1 , 2, 3, 4, 5, 6, 7, 8, 9};
     int sum = 0;
     int x = 0;
 
+    // Incorrectly unrecognized.
     for(int i = 0; i < ARRAYSIZE; i++){
-        sum += array[i];//sim
-    }//done
+        sum += array[i];
+    }
 
+    // Correctly unrecognized.
     for(int i = 0; i < ARRAYSIZE; i++){
-        sum += sum + array[i];//nao
-    }//done
+        sum += sum + array[i]; // Note that sum is being added to sum.
+    }
 
+    // Incorrectly unrecognized.
     for(int i = 0; i < ARRAYSIZE; i++){
-        sum -= array[i];//sim
-    }//done
+        sum -= array[i];
+    }
 
+    // Correctly recognized.
     for(int i = 0; i < ARRAYSIZE; i++){
         x = x + array[i];//sim
     }//done
 
+    // Correctly unrecognized.
     for(int i = 0; i < ARRAYSIZE; i++){
-        sum = x + array[i];//nao
-    }//done
+        sum = x + array[i];
+    }
 
+    // Correctly recognized.
     for(int i = 0; i < ARRAYSIZE; i++){    
-        sum = x + sum + array[i];//sim
-    }//done
+        sum = x + sum + array[i];
+    }
 
-    for(int i = 0; i < ARRAYSIZE; i++){//nao pode, pois x depende de sum, e nada alem de sum pode depender de sum
+    // Correctly unrecognized. ("x depende de sum, e nada alem de sum pode depender de sum")
+    for(int i = 0; i < ARRAYSIZE; i++){
         x = sum;
         sum = sum + x;
-    }//not done
-
+    }
+    
+    // Correctly recognized. // TODO: should this really be recognized?
     for(int i = 0; i < ARRAYSIZE; i++){
         sum = sum + array[i];//sim//done
         sum = sum - array[i];//sim//done
         sum = array[i] + sum;//sim//done
     }//done
 
-    for(int i = 0; i < ARRAYSIZE; i++){//sim
+    // Correctly recognized.
+    for(int i = 0; i < ARRAYSIZE; i++){
         sum = sum * array[i];
-    }//done
+    }
 
-    for(int i = 0; i < ARRAYSIZE; i++){//sim, mesmo motivo do x
-        sum = sum + array[i+1];
-    }//done
-    int j = 0;
-    for(int i = 0; i < ARRAYSIZE; i++){//sim, mesmo motivo do x
-        sum = sum + array[j];
-    }//done
-
-    for(int i = 0; i < ARRAYSIZE; i++){//sim
+    // Correctly recognized.
+    for(int i = 0; i < ARRAYSIZE; i++){
         sum = sum / array[i];
-    }//done
+    }
 
-    for(int i = 0; i < ARRAYSIZE; i++){//nao
+    // Correctly unrecognized.
+    for(int i = 0; i < ARRAYSIZE; i++){
         sum  = sum + sum;
-    }//done
+    }
 
-    for(int i = 0; i < ARRAYSIZE; i++){//sim, pois x replicado pode ser considerado uma string, a nao ser que ele receba sum em alguma parte do for
+    // Correctly recognized. ("sim, pois x replicado pode ser considerado uma string, a nao ser que ele receba sum em alguma parte do for")
+    for(int i = 0; i < ARRAYSIZE; i++){
         sum  = sum + x;
-    }//done
+    }
+    
+    // Correctly recognized. ("mesmo motivo do x")
+    for(int i = 0; i < ARRAYSIZE; i++){
+        sum = sum + array[i+1];
+    }
+    
+    // Correctly recognized. ("mesmo motivo do x")
+    int j = 0;
+    for(int i = 0; i < ARRAYSIZE; i++){
+        sum = sum + array[j];
+    }
 
-    for(int i = 0; i < ARRAYSIZE; i++){//nao
+    // Correctly unrecognized.
+    for(int i = 0; i < ARRAYSIZE; i++){
         sum = sum + array[i] + sum;
-    }//done
-/*
+    }
+/*  // TODO
     for(){
         sum = fun(sum, array[i])
     }
