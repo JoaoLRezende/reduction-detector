461958db3e8768af245d36adf2ffefb9480c3904
João Ladeira Rezende <joaozinhobent@gmail.com>
2020-10-09 07:34:56 -0300

    Format with clang-format

--- reduction/reduction.cpp
+++ reduction/reduction.cpp
@@ -7,9 +7,9 @@
 // Declares llvm::cl::extrahelp.
 #include "llvm/Support/CommandLine.h"
 
-#include "clang/ASTMatchers/ASTMatchers.h"
-#include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/AST/ASTContext.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
 
 using namespace clang;
 using namespace clang::ast_matchers;
@@ -28,6 +28,11 @@ static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 // A help message for this specific tool can be added afterwards.
 static cl::extrahelp MoreHelp("\nMore help text...");
 
+/* TODO: this function isn't used anymore. But we might still learn something
+ * from it: should we be using getCanonicalDecl when comparing declarations?
+ * Study this, consider using this in the new code, and then get rid of this
+ * function.
+ */
 static bool areSameVariable(const ValueDecl *First, const ValueDecl *Second) {
   return First && Second &&
          First->getCanonicalDecl() == Second->getCanonicalDecl();
@@ -39,20 +44,15 @@ static bool areSameVariable(const ValueDecl *First, const ValueDecl *Second) {
  * For example: sum = sum + array[i]
  */
 StatementMatcher reduceSimpleAssignmentMatcher1 =
-  binaryOperator(
-    hasOperatorName("="),
-    hasLHS(
-      declRefExpr(to(varDecl().bind("accumulator")))), 
-    
-    hasRHS(
-      binaryOperator(
-        hasLHS(
-          hasDescendant(
-            declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))), 
-        unless(hasRHS(
-          hasDescendant(
-            declRefExpr(to(varDecl(equalsBoundNode("accumulator"))))))))) 
-  ).bind("reduce");
+    binaryOperator(hasOperatorName("="),
+                   hasLHS(declRefExpr(to(varDecl().bind("accumulator")))),
+
+                   hasRHS(binaryOperator(
+                       hasLHS(hasDescendant(declRefExpr(
+                           to(varDecl(equalsBoundNode("accumulator")))))),
+                       unless(hasRHS(hasDescendant(declRefExpr(
+                           to(varDecl(equalsBoundNode("accumulator"))))))))))
+        .bind("reduce");
 
 /* A matcher that matches any assignment whose right-hand side
  * is a binary operation whose second operand contains the assignee,
@@ -60,17 +60,15 @@ StatementMatcher reduceSimpleAssignmentMatcher1 =
  * For example: sum = array[i] + sum
  */
 StatementMatcher reduceSimpleAssignmentMatcher2 =
-  binaryOperator(
-    hasOperatorName("="),
-    hasLHS(
-      declRefExpr(to(varDecl().bind("accumulator")))), 
-    
-    hasRHS(
-      binaryOperator(
-        hasRHS(
-            ignoringParenImpCasts(
-             declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))))), 
-    unless(hasDescendant(binaryOperator(hasLHS(hasDescendant(declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))))))).bind("reduce");
+    binaryOperator(
+        hasOperatorName("="),
+        hasLHS(declRefExpr(to(varDecl().bind("accumulator")))),
+
+        hasRHS(binaryOperator(hasRHS(ignoringParenImpCasts(
+            declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))))),
+        unless(hasDescendant(binaryOperator(hasLHS(hasDescendant(
+            declRefExpr(to(varDecl(equalsBoundNode("accumulator"))))))))))
+        .bind("reduce");
 
 /* TODO: the two matchers above seem to be an attempt to exclude assignments
  * whose assignee appear in their right-hand side multiple times.
@@ -85,58 +83,59 @@ StatementMatcher reduceSimpleAssignmentMatcher2 =
  * sum = sum + sum + x
  * since the plus operator is left-associative.
  * There should be a better way to do this.
- * Maybe we can bind the first reference to the potential accumulator we find in the right-hand
+ * Maybe we can bind the first reference to the potential accumulator we find in
+ * the right-hand
  * side to a name and then see if there is still any reference to that potential
  * accumulator in that same right-hand side that _isn't_ bound to that name.
  * (This might require some procedural code.)
  */
 
-/* A matcher that matches a compound assignment whose left-hand side is a variable
+/* A matcher that matches a compound assignment whose left-hand side is a
+ * variable
  * that does not appear in its right-hand side.
  */
-StatementMatcher reduceCompoundAssignmentMatcher = 
-  binaryOperator(
-    anyOf(hasOperatorName("+="), hasOperatorName("-="), hasOperatorName("*="), hasOperatorName("/=")), 
-    
-    hasLHS(
-      declRefExpr(to(varDecl().bind("accumulator")))),
-    unless(hasRHS(
-      hasDescendant(
-        declRefExpr(to(varDecl(equalsBoundNode("accumulator")))))))
-  ).bind("reduce");
+StatementMatcher reduceCompoundAssignmentMatcher =
+    binaryOperator(anyOf(hasOperatorName("+="), hasOperatorName("-="),
+                         hasOperatorName("*="), hasOperatorName("/=")),
 
-StatementMatcher reduceAssignmentMatcher = 
-    findAll(stmt(
-        anyOf(
-            reduceSimpleAssignmentMatcher1,
-            reduceSimpleAssignmentMatcher2,
-            reduceCompoundAssignmentMatcher // TODO: bind the matching node to a name here (and use a more reasonable name) rather than in the inner matchers.
-        )
-    ));
+                   hasLHS(declRefExpr(to(varDecl().bind("accumulator")))),
+                   unless(hasRHS(hasDescendant(declRefExpr(
+                       to(varDecl(equalsBoundNode("accumulator"))))))))
+        .bind("reduce");
+
+StatementMatcher reduceAssignmentMatcher = findAll(
+    stmt(anyOf(reduceSimpleAssignmentMatcher1, reduceSimpleAssignmentMatcher2,
+               reduceCompoundAssignmentMatcher)));
+/* TODO: bind the matching node to a name here (and use a more reasonable name)
+ * rather than in the inner matchers.
+ */
 
 // old. remove after recycling.
 class LoopPrinter : public MatchFinder::MatchCallback {
-public :
+public:
   virtual void run(const MatchFinder::MatchResult &Result) {
-      ASTContext *Context = Result.Context;
-      llvm::outs() << "Hello!!\n";
-      const ForStmt *FS = Result.Nodes.getNodeAs<ForStmt>("forLoop");
-      // We do not want to convert header files!
-      if(!FS || !Context->getSourceManager().isWrittenInMainFile(FS->getForLoc()))
-        return;
-      const VarDecl *IncVar = Result.Nodes.getNodeAs<VarDecl>("incVarName");
-      const VarDecl *CondVar = Result.Nodes.getNodeAs<VarDecl>("condVarName");
-      //const VarDecl *InitVar = Result.Nodes.getNodeAs<VarDecl>("initVarName");
+    ASTContext *Context = Result.Context;
+    llvm::outs() << "Hello!!\n";
+    const ForStmt *FS = Result.Nodes.getNodeAs<ForStmt>("forLoop");
+    // We do not want to convert header files!
+    if (!FS ||
+        !Context->getSourceManager().isWrittenInMainFile(FS->getForLoc()))
+      return;
+    const VarDecl *IncVar = Result.Nodes.getNodeAs<VarDecl>("incVarName");
+    const VarDecl *CondVar = Result.Nodes.getNodeAs<VarDecl>("condVarName");
+    // const VarDecl *InitVar = Result.Nodes.getNodeAs<VarDecl>("initVarName");
 
-      if(!areSameVariable(IncVar, CondVar)/* || !areSameVariable(IncVar, InitVar)*/){
-        llvm::outs() << "for variables didnt match\n";
-        return;
-      }
-      FS->dump();
-      llvm::outs() << "Potential Reduce pattern, in the next line you'll find the file and the suspect line.\n";
-      FS->getForLoc().dump(Context->getSourceManager());
-      llvm::outs() << "\n";
+    if (!areSameVariable(IncVar,
+                         CondVar) /* || !areSameVariable(IncVar, InitVar)*/) {
+      llvm::outs() << "for variables didnt match\n";
+      return;
     }
+    FS->dump();
+    llvm::outs() << "Potential Reduce pattern, in the next line you'll find "
+                    "the file and the suspect line.\n";
+    FS->getForLoc().dump(Context->getSourceManager());
+    llvm::outs() << "\n";
+  }
 };
 
 StatementMatcher loopMatcher = forStmt().bind("forLoop");
@@ -146,6 +145,33 @@ StatementMatcher loopMatcher = forStmt().bind("forLoop");
  * It checks whether a for loop looks like a reduction operation.
  */
 class LoopChecker : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &result) {
+    ASTContext *context = result.Context;
+
+    const ForStmt *forStmt = result.Nodes.getNodeAs<ForStmt>("forLoop");
+
+    llvm::outs() << "Found a for loop in the following location:\n";
+    forStmt->getForLoc().dump(context->getSourceManager());
+    forStmt->dumpPretty(*result.Context);
+
+    // We do not want to scan header files.
+    if (!forStmt ||
+        !context->getSourceManager().isWrittenInMainFile(forStmt->getForLoc()))
+      return;
+
+    /*
+     * TODO: for each assignment that looks like a reduction assignment,
+     * check whether its lvalue
+     * is referenced in any other expression of the loop.
+     * If it isn't, then report it as a possible reduction accumulator.
+     */
+    AccumulatorChecker accumulatorChecker(forStmt);
+    MatchFinder expressionFinder;
+    expressionFinder.addMatcher(reduceAssignmentMatcher, &accumulatorChecker);
+    expressionFinder.match(*forStmt, *context);
+  }
+
   /*
    * In a for loop, for each assignment that looks like a possible
    * reduction assignment (e.g. sum += array[i]), we check whether
@@ -155,59 +181,33 @@ class LoopChecker : public MatchFinder::MatchCallback {
    */
   class AccumulatorChecker : public MatchFinder::MatchCallback {
 
+    /* One instance of AccumulatorChecker is created for each for loop.
+     * A reference to the for loop an instance was created to search
+     * is kept in forStmt.
+     */
     const ForStmt *forStmt;
 
   public:
-
-    AccumulatorChecker(const ForStmt *forStmt) {
-      this->forStmt = forStmt;
-    }
+    AccumulatorChecker(const ForStmt *forStmt) { this->forStmt = forStmt; }
 
     virtual void run(const MatchFinder::MatchResult &result) {
       // Get the matched assignment.
-      const BinaryOperator *assignment = result.Nodes.getNodeAs<BinaryOperator>("reduce");
+      const BinaryOperator *assignment =
+          result.Nodes.getNodeAs<BinaryOperator>("reduce");
       llvm::errs() << "Possible reduction assignment: ";
       assignment->dumpPretty(*result.Context);
       llvm::errs() << "\n";
 
       // Get the assignment's assignee.
-      const VarDecl *possibleAccumulator = result.Nodes.getNodeAs<VarDecl>("accumulator");
+      const VarDecl *possibleAccumulator =
+          result.Nodes.getNodeAs<VarDecl>("accumulator");
 
       // Construct a matcher that matches other references to the assignee.
       StatementMatcher outsideReferenceMatcher =
-        declRefExpr(
-          to(varDecl(equalsNode(possibleAccumulator))),
-          unless(hasAncestor(equalsNode(assignment)))
-        );
+          declRefExpr(to(varDecl(equalsNode(possibleAccumulator))),
+                      unless(hasAncestor(equalsNode(assignment))));
     };
   };
-
-  public:
-
-    virtual void run(const MatchFinder::MatchResult &result) {
-      ASTContext *context = result.Context;
-
-      const ForStmt *forStmt = result.Nodes.getNodeAs<ForStmt>("forLoop");
-
-      llvm::outs() << "Found a for loop in the following location:\n";
-      forStmt->getForLoc().dump(context->getSourceManager());
-      forStmt->dumpPretty(*result.Context);
-
-      // We do not want to scan header files.
-      if(!forStmt || !context->getSourceManager().isWrittenInMainFile(forStmt->getForLoc()))
-        return;
-
-      /*
-       * TODO: for each assignment that looks like a reduction assignment,
-       * check whether its lvalue
-       * is referenced in any other expression of the loop.
-       * If it isn't, then report it as a possible reduction accumulator.
-       */
-      AccumulatorChecker accumulatorChecker(forStmt);
-      MatchFinder expressionFinder;
-      expressionFinder.addMatcher(reduceAssignmentMatcher, &accumulatorChecker);
-      expressionFinder.match(*forStmt, *context);
-    }
 };
 
 int main(int argc, const char **argv) {
@@ -228,27 +228,36 @@ int main(int argc, const char **argv) {
  *   RESTO DA EQUACAO, E DEPOIS VERIFICAR SE APARECE NOVAMENTE EM ALGUMA
  *   EQUACAO DENTRO DO FOR. MAS COMO GUARDAR ONDE DA EQUACAO/FOR QUE
  *   ENCONTREI O ACUMULADOR EM QUESTAO?
- * - At the end of a run, print something like "__ out of __ loops recognized as possible reductions".
+ * - At the end of a run, print something like "__ out of __ loops recognized as
+ *   possible reductions".
  *   That should facilitate checking whether all loops were recognized.
  * - Get more example loops from real software systems. (See PARSEC, Cowichan.)
  * - How well do we deal with nested loops? Write some test cases for that.
- * - Optionally print detected loops themselves, rather than simply their locations.
- * - Be able to receive a directory as input (rather than only a single file). Then, test on
- *   larger software systems (such as Linux).
+ * - Optionally print detected loops themselves, rather than simply their
+ *   locations.
+ * - Be able to receive a directory as input (rather than only a single file).
+ *   Then, test on larger software systems (such as Linux).
  * - Comment the code.
- * - Fix indentation. Use deeper indentation to make reading easier. At least 4 spaces.
- * - Consider that a reduction assignment can involve a function rather than a raw arithmetic operator: sum = f(sum, array[i]).
- * - If we plan to work with C++ too, we'll probably have to deal with other ways of iterating over an array (like range-based for loops)
+ * - Fix indentation. Use deeper indentation to make reading easier. At least 4
+ *   spaces.
+ * - Consider that a reduction assignment can involve a function rather than a
+ *   raw arithmetic operator: sum = f(sum, array[i]).
+ * - If we plan to work with C++ too, we'll probably have to deal with other
+ *   ways of iterating over an array (like range-based for loops)
  *   and with collections other than arrays.
- * - What happens when we have multiple possible accumulators (i.e. multiple nodes bound to the name "accumulator"?) Add test cases. Deal with that.
- * - Make a basic testing framework that allows a good number of regression tests without requiring laborious output-checking effort.
-     Each loop would go in a separate function with a unique name. Each function would be named either reduce<n> or notAReduce<n>,
-     where <n> is an integer. The main function would then simply check whether each loop is or is not detected in accordance with
-     the enclosing function's name. (It probably is trivial to get the name of the function in which a loop was detected.)
-     Then, collect all tests in a single file.
-   - Should we recognize loops like the following as reductions?
-        for (int i = 0; i < arr_length; i++)
-            if (max < arr[i])
-                max = arr[i];
-
+ * - What happens when we have multiple possible accumulators (i.e. multiple
+ *   nodes bound to the name "accumulator"?) Add test cases. Deal with that.
+ * - Make a basic testing framework that allows a good number of regression
+ *   tests without requiring laborious output-checking effort.
+ *   Each loop would go in a separate function with a unique name. Each function
+ *   would be named either reduce<n> or notAReduce<n>,
+ *   where <n> is an integer. The main function would then simply check whether
+ *   each loop is or is not detected in accordance with
+ *   the enclosing function's name. (It probably is trivial to get the name of
+ *   the function in which a loop was detected.)
+ *   Then, collect all tests in a single file.
+ * - Should we recognize loops like the following as reductions?
+ *      for (int i = 0; i < arr_length; i++)
+ *          if (max < arr[i])
+ *              max = arr[i];
  */
