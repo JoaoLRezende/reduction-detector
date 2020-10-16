c78ddbbbebd9e54e4541dcc0c68f28bc3e116722
João Ladeira Rezende <joaozinhobent@gmail.com>
2020-10-16 09:32:37 -0300

    Check program for syntactic errors before build
    Actually don't, because running clang-check here fails for some
    reason. But I'm leaving that command line as a comment because
    I plan to try to fix that later.

--- reduction/make.sh
+++ reduction/make.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-#copy and make
-cp reduction.cpp ../llvm/tools/clang/tools/extra/reduction
+# copy and make
+
+cp reduction.cpp ../llvm/tools/clang/tools/extra/reduction &&
+
+# Check the program for syntactic errors before starting the build process.
+#../build/bin/clang-check ../llvm/tools/clang/tools/extra/reduction/reduction.cpp &&    # segfaults, for some reason
+
 make -j reduction -C ../build/ --no-print-directory
+
 echo "Done"
+
