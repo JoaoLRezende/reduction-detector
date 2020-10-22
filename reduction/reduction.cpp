2d000b9bd2a659977075838bba33faf46106b858
João Ladeira Rezende <joaozinhobent@gmail.com>
2020-10-22 16:40:47 -0300

    Be able to receive directories

--- reduction/reduction.cpp
+++ reduction/reduction.cpp
@@ -1,3 +1,15 @@
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <dirent.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 // Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
 
@@ -191,10 +203,75 @@ public:
   };
 };
 
+
+/*
+ * todo: description.
+ * Concatenates those paths to output_list.
+ */
+static void get_files_in_directory(std::string &directory_path,
+                                   std::vector<std::string> &output_list) {
+  if (directory_path.back() == '/')
+    directory_path.pop_back();
+
+  DIR *directory = opendir(directory_path.c_str());
+  struct stat stat_struct;
+  // for each file in the directory
+  for (struct dirent *directory_entry = readdir(directory);
+       directory_entry != nullptr; directory_entry = readdir(directory)) {
+    std::string path = directory_path + "/" + directory_entry->d_name;
+    if (stat(path.c_str(), &stat_struct)) {
+      perror(("couldn't stat file " + path).c_str());
+    }
+
+    if ((stat_struct.st_mode & S_IFMT) == S_IFREG) { // if is a regular file
+      output_list.push_back(path);
+    } else if ((stat_struct.st_mode & S_IFMT) == S_IFDIR &&
+               strcmp(".", directory_entry->d_name) &&
+               strcmp("..", directory_entry->d_name)) { // if is a directory
+      get_files_in_directory(path, output_list);
+    }
+  }
+  closedir(directory);
+}
+
+void expand_directories(std::vector<std::string> &input_list,
+                        std::vector<std::string> &output_list) {
+  for (std::string &path : input_list) {
+    struct stat stat_struct;
+    if (stat(path.c_str(), &stat_struct)) {
+      perror(("couldn't stat file " + path).c_str());
+    }
+
+    if ((stat_struct.st_mode & S_IFMT) == S_IFREG) { // if is a regular file
+      output_list.push_back(path);
+    } else if ((stat_struct.st_mode & S_IFMT) == S_IFDIR) { // if is a directory
+      get_files_in_directory(path, output_list);
+    }
+  }
+}
+
+void dump_string_vector(std::vector<std::string> &vector) {
+  for (std::string &string : vector) {
+    llvm::errs() << "-- " << string << "\n";
+  }
+}
+
 int main(int argc, const char **argv) {
   CommonOptionsParser optionsParser(argc, argv, myToolCategory);
-  ClangTool clangTool(optionsParser.getCompilations(),
-                      optionsParser.getSourcePathList());
+
+  std::vector<std::string> input_path_list = optionsParser.getSourcePathList();
+  llvm::errs() << "input_path_list: ";
+  dump_string_vector(input_path_list);
+  llvm::errs() << "\n";
+
+  std::vector<std::string> expanded_path_list;
+  expand_directories(input_path_list, expanded_path_list);
+
+  llvm::errs() << "expanded_path_list:\n";
+  dump_string_vector(expanded_path_list);
+  llvm::errs() << "\n";
+
+  ClangTool clangTool(optionsParser.getCompilations(), expanded_path_list);
 
   LoopChecker loopChecker;
   MatchFinder finder;
@@ -268,4 +345,5 @@ int main(int argc, const char **argv) {
  *          if (max < arr[i])
  *              max = arr[i];
  * - Separate the code into multiple source files.
+ * - Run Valgrind. Make sure we're not leaking memory.
  */
