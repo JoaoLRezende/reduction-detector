a8d7d562dbc013698b58829a9c341b102765613b
João Ladeira Rezende <jplrezende@inf.ufpel.edu.br>
2020-11-12 10:39:40 -0300

    During setup, don't complain if build directory exists

--- reduction-detector/set-up-build.sh
+++ reduction-detector/set-up-build.sh
@@ -1,7 +1,7 @@
 # Create the build directory and execute CMake.
 cd "$(dirname "$0")" &&
 cd .. &&
-mkdir build &&
+mkdir -p build &&
 cd build &&
 ../cmake/bin/cmake -G Ninja -DCMAKE_MAKE_PROGRAM=../ninja/ninja ../llvm-project/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_BUILD_TESTS=ON
 
