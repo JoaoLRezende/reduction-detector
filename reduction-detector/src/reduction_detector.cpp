a5a399a72bee2bef7313c45732691a8197e2b837
João Ladeira Rezende <jplrezende@inf.ufpel.edu.br>
2020-11-18 14:28:14 -0300

    Reorganize reduction_detector.cpp

--- reduction-detector/src/reduction_detector.cpp
+++ reduction-detector/src/reduction_detector.cpp
@@ -2,33 +2,16 @@
 #include <string>
 #include <vector>
 
-// Declares clang::SyntaxOnlyAction.
-#include "clang/Frontend/FrontendActions.h"
-
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-
-// Declares llvm::cl::extrahelp.
-#include "llvm/Support/CommandLine.h"
-
-#include "llvm/Support/raw_ostream.h"
-
-#include "clang/AST/PrettyPrinter.h"
-#include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
-
-using namespace clang;
-using namespace clang::ast_matchers;
-using namespace clang::tooling;
-using namespace llvm;
 
 #include "reduction-detector/translation_unit_finder.h"
 using reduction_detector::translation_unit_finder::expand_directories;
 using reduction_detector::translation_unit_finder::dump_string_vector;
-
-#include "reduction-detector/reduction_assignment_matchers.h"
-using namespace reduction_detector::reduction_assignment_matchers;
 
 #include "reduction-detector/loop_analysis.h"
 using reduction_detector::loop_analysis::LoopChecker;
@@ -40,14 +23,16 @@ static llvm::cl::OptionCategory myToolCategory("my-tool options");
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
 // It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static llvm::cl::extrahelp
+    commonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...");
+static llvm::cl::extrahelp MoreHelp("\nMore help text...");
 
 int main(int argc, const char **argv) {
-  CommonOptionsParser optionsParser(argc, argv, myToolCategory);
+  clang::tooling::CommonOptionsParser optionsParser(argc, argv, myToolCategory);
 
+  // TODO: move these debugging-related statements into translation_unit_finder.cpp, and then make all of that module's functions private except the one called here.
   std::vector<std::string> input_path_list = optionsParser.getSourcePathList();
   llvm::errs() << "input_path_list:\n";
   dump_string_vector(input_path_list);
@@ -60,13 +45,16 @@ int main(int argc, const char **argv) {
   dump_string_vector(expanded_path_list);
   llvm::errs() << "\n";
 
-  ClangTool clangTool(optionsParser.getCompilations(), expanded_path_list);
+  clang::tooling::ClangTool clangTool(optionsParser.getCompilations(),
+                                      expanded_path_list);
 
   LoopChecker loopChecker;
-  MatchFinder finder;
-  finder.addMatcher(forStmt().bind("forLoop"), &loopChecker);
+  clang::ast_matchers::MatchFinder finder;
+  finder.addMatcher(clang::ast_matchers::forStmt().bind("forLoop"),
+                    &loopChecker);
 
-  int statusCode = clangTool.run(newFrontendActionFactory(&finder).get());
+  int statusCode =
+      clangTool.run(clang::tooling::newFrontendActionFactory(&finder).get());
 
   llvm::errs() << loopChecker.likelyReductionCount << " out of "
                << loopChecker.totalLoopCount
