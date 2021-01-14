56966057b3bee0ac3a45a7e749379ca6ef13847c
João Rezende <joaozinhobent@gmail.com>
2021-01-14 10:04:21 -0300

    Make a very minor formatting change in test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 if [ -z "$1" ]
   then
     echo "Testing on $DEFAULT_TEST_CASE."
-    build/reduction-detector $EXTRA_ARGUMENTS $DEFAULT_TEST_CASE --
+    build/reduction-detector $EXTRA_ARGUMENTS $DEFAULT_TEST_CASE  --
   else
-    build/reduction-detector $EXTRA_ARGUMENTS $1 --
+    build/reduction-detector $EXTRA_ARGUMENTS $1                  --
 fi
