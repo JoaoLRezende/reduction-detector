d77fac5c8fe5fb00501666f8a700f77960997006
João Rezende <joaozinhobent@gmail.com>
2021-02-23 13:40:21 -0300

    Remove outdated comments

--- src/command_line.cpp
+++ src/command_line.cpp
@@ -11,14 +11,5 @@ namespace command_line_options {
 llvm::cl::OptionCategory
     reduction_detector_option_category("reduction-detector options");
 
-// A help message for this specific tool can be added afterwards.
-// TODO: consider eliminating this, and honoring the instructions in
-// CommandLine's user guide to do this instead. Those instructions allow your
-// short help text to appear at the top of the help output, which is even
-// better.
-// static llvm::cl::extrahelp MoreHelp("\nMore help text...");
 }
 }
-
-// TODO: change the name of this module (including the corresponding implementation
-// file) to simply "command_line".
