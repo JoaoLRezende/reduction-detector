346ca35f5d16dc3e5a6448a0d70aaaa6c6b5e740
João Rezende <joaozinhobent@gmail.com>
2021-04-03 16:56:54 -0300

    Refactor detection of trivial accumulators

--- src/loop_analysis/determine_trivial_accumulators.cpp
+++ src/loop_analysis/determine_trivial_accumulators.cpp
@@ -7,16 +7,16 @@ namespace internal {
 
 void determineTrivialAccumulators(PossibleReductionLoopInfo &loop_info) {
   for (auto &possible_accumulator : loop_info.possible_accumulators) {
-    if ((possible_accumulator.second.possible_accumulating_assignments.size() !=
+    if ((possible_accumulator.second.possible_accumulating_assignments.size() ==
          possible_accumulator.second
-             .number_of_possible_accumulating_assignments_whose_RHS_also_references_this) ||
-        possible_accumulator.second.outside_references > 0) {
+             .number_of_possible_accumulating_assignments_whose_RHS_also_references_this) &&
+        possible_accumulator.second.outside_references == 0) {
+      possible_accumulator.second.is_trivial_accumulator = true;
+    } else {
       possible_accumulator.second.is_trivial_accumulator = false;
       if (possible_accumulator.second.isLikelyAccumulator) {
         loop_info.has_likely_but_non_trivial_accumulator = true;
       }
-    } else {
-      possible_accumulator.second.is_trivial_accumulator = true;
     }
   }
 }
