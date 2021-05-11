ed0c2b86af5ed4be311468c60bfb5acea4779ead
João Rezende <joaozinhobent@gmail.com>
2021-05-11 09:04:09 -0300

    Change test.sh to also generate verbose output

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DEFAULT_TEST_CASE="test inputs/test.c"
+TEST_CASE="test inputs/reduction loops"
 
-EXTRA_ARGUMENTS="--output test.out --min-score 7 --only-non-trivial-reductions --verbose"
+EXTRA_ARGUMENTS="--only-non-trivial-reductions"
 
 # CLANG_ARGUMENTS can be used to inform the tool of where to find system header
 # files (for example, if you see errors like "fatal error: 'stddef.h' file
@@ -10,11 +10,12 @@ EXTRA_ARGUMENTS="--output test.out --min-score 7 --only-non-trivial-reductions -
 CLANG_ARGUMENTS="-I /usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include"
 
 cd "$(dirname "$0")"
-
-if [ -z "$1" ]
-  then
-    echo "Testing on $DEFAULT_TEST_CASE."
-    build/reduction-detector $EXTRA_ARGUMENTS "$DEFAULT_TEST_CASE"  -- $CLANG_ARGUMENTS
-  else
-    build/reduction-detector $EXTRA_ARGUMENTS "$1"                  -- $CLANG_ARGUMENTS
-fi
+echo "Testing on $TEST_CASE."
+# normal output
+build/reduction-detector  --output test.out \
+                          $EXTRA_ARGUMENTS "$TEST_CASE" \
+                          -- $CLANG_ARGUMENTS
+# verbose output
+build/reduction-detector --verbose --output test_verbose.out \
+                         $EXTRA_ARGUMENTS "$TEST_CASE" \
+                         -- $CLANG_ARGUMENTS
