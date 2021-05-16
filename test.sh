c114b658fa66ef47e84e359e302923fd1fea87b1
João Rezende <joaozinhobent@gmail.com>
2021-05-16 10:09:23 -0300

    Move test-output files into dedicated directory

--- test.sh
+++ test.sh
@@ -2,20 +2,22 @@
 
 TEST_CASE="test cases/reduction loops"
 
-EXTRA_ARGUMENTS="--only-non-trivial-reductions"
+ADDITIONAL_ARGUMENTS="--only-non-trivial-reductions"
 
 # CLANG_ARGUMENTS can be used to inform the tool of where to find system header
 # files (for example, if you see errors like "fatal error: 'stddef.h' file
 # not found").
 CLANG_ARGUMENTS="-I /usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include"
 
+OUTPUT_DIRECTORY="test output"
+
 cd "$(dirname "$0")"
 echo "Testing on $TEST_CASE."
 # normal output
-build/reduction-detector  --output test.out \
-                          $EXTRA_ARGUMENTS "$TEST_CASE" \
+build/reduction-detector  --output "${OUTPUT_DIRECTORY}/test.out" \
+                          $ADDITIONAL_ARGUMENTS "$TEST_CASE" \
                           -- $CLANG_ARGUMENTS
 # verbose output
-build/reduction-detector --verbose --output test_verbose.out \
-                         $EXTRA_ARGUMENTS "$TEST_CASE" \
+build/reduction-detector --verbose --output "${OUTPUT_DIRECTORY}/test_verbose.out" \
+                         $ADDITIONAL_ARGUMENTS "$TEST_CASE" \
                          -- $CLANG_ARGUMENTS
