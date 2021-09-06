c14e5419246fef198359fe53f5f82d5548a34f71
João Rezende <joaozinhobent@gmail.com>
2021-09-06 11:52:57 -0300

    In tests, also print unlikely reduction loops

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+arguments="--print-non-reduction-loops --report-unlikely-accumulators"
+
 # CLANG_ARGUMENTS can be used to inform the tool of where to find system header
 # files (for example, if you see errors like "fatal error: 'stddef.h' file
 # not found").
@@ -15,16 +17,15 @@ function execute_on_file {
     local file="$1"
     local normal_output_file="$2"
     local verbose_output_file="$3"
-    local additional_arguments="$4"
 
     echo "Testing on $file."
     # normal output
     build/reduction-detector  --output "${OUTPUT_DIRECTORY}/$normal_output_file" \
-                            $additional_arguments "$file" \
+                            $arguments "$file" \
                             -- $CLANG_ARGUMENTS
     # verbose output
     build/reduction-detector --verbose --output "${OUTPUT_DIRECTORY}/$verbose_output_file" \
-                            $additional_arguments "$file" \
+                            $arguments "$file" \
                             -- $CLANG_ARGUMENTS
 }
 
@@ -32,6 +33,5 @@ execute_on_file "test cases/reduction loops/artificial examples.c" \
                 "positive_test_cases.out" "positive_test_cases_verbose.out"
 
 execute_on_file "test cases/non-reduction loops/artificial non-examples.c" \
-                "negative_test_cases.out" "negative_test_cases_verbose.out" \
-                "--print-non-reduction-loops --report-unlikely-accumulators"
+                "negative_test_cases.out" "negative_test_cases_verbose.out"
                 
